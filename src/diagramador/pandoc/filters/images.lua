519585d2482b03c93c13857400a4c7df197318fd
Gabriel Braun <braun.pineschi@gmail.com>
2024-01-25 21:07:11 -0300

    pega o nome da imagem

--- src/diagramador/pandoc/filters/images.lua
+++ src/diagramador/pandoc/filters/images.lua
@@ -21,6 +21,7 @@ local function Image(elem)
     -- Substituir um .pdf referente a um .svg.
     --
     local img_name, img_ext = pandoc.path.split_extension(elem.src)
+
     if img_ext == ".svg" then
         local svg_path = pandoc.path.join { src_path, img_name .. ".svg" }
         local pdf_path = pandoc.path.join { tmp_path, img_name .. ".pdf" }
@@ -28,7 +29,10 @@ local function Image(elem)
         os.execute("cairosvg -s 1.33 " .. svg_path .. " -o " .. pdf_path)
 
         elem.src = pdf_path
+    else
+        elem.src = pandoc.path.filename(elem.src)
     end
+
     return elem
 end
 
